c6541f543b35d0811448fc2a18a12a4fcda192fa
NouriAdel <47252364+NouriAdel@users.noreply.github.com>
2019-06-22 14:24:30 +0200

    Create VBoxExperiments.java

---
+++ src/STAMP/VBoxExperiments.java
@@ -0,0 +1,30 @@
+package STAMP;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class VBoxExperiments extends Application  {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("HBox Experiment 1");
+
+        Button button1 = new Button("Button Number 1");
+        Button button2 = new Button("Button Number 2");
+
+        VBox vbox = new VBox(button1, button2);
+
+        Scene scene = new Scene(vbox, 200, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
