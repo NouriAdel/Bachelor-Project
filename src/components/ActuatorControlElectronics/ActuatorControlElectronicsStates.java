c9a83f9c0e33ca054a610b4f72d53a80896af2b5
NouriAdel <47252364+NouriAdel@users.noreply.github.com>
2019-06-22 14:19:28 +0200

    Create ActuatorControlElectronicsStates.java

---
+++ src/components/ActuatorControlElectronics/ActuatorControlElectronicsStates.java
@@ -0,0 +1,5 @@
+package components.ActuatorControlElectronics;
+
+public enum ActuatorControlElectronicsStates {
+	NOMINAL, LOSS_OF_POWER_SUPPLY, LOSS_OR_INVALID_CONTROL_SIGNAL, LOSS_OF_INPUT_SIGNAL
+}
